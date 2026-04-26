3fd5468935343d40e43ded0dcfc8c39dc866dc3e
Charles Plett <1403286+funkypc@users.noreply.github.com>
2026-04-25 19:22:18 -0500

    fix dependency conflict

--- service/kotlinYtmusicScraper/src/commonMain/kotlin/com/maxrave/kotlinytmusicscraper/YouTube.kt
+++ service/kotlinYtmusicScraper/src/commonMain/kotlin/com/maxrave/kotlinytmusicscraper/YouTube.kt
@@ -1290,7 +1290,7 @@ class YouTube {
                         cpn,
                         signatureTimestamp =
                             run {
-                                val today = kotlinx.datetime.Clock.System.todayIn(TimeZone.UTC)
+                                val today = Clock.System.todayIn(TimeZone.UTC)
                                 val epoch =
                                     Instant
                                         .fromEpochSeconds(0)
